2c2496a96ee0422ae26a417460698a65bc5a6889
Sebastian Nicolas <snicolas@analytia.pe>
2025-11-15 08:59:05 -0500

    agregar layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [LayoutComponent],
+  template: '<app-layout />',
+  styles: ``,
 })
 export class App {
   protected readonly title = signal('oss-new');
